1e3b9ff960eb36458fe26f4e95fe53ec54e7230e
Jonathan Hogg <jhogg41@gmail.com>
2015-05-17 11:14:56 +0100

    Get basic routing to work

---
+++ gom_server/core/router.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Register actual routes when called by master urls.py
+def addRoutes(router):
+   router.register(r'users', UserViewSet)
