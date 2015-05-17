cf58db160e9099685a9dd70769d6d8f5795cdab3
Jonathan Hogg <jhogg41@gmail.com>
2015-05-17 10:51:14 +0100

    Basic migrations sorted

---
+++ gom_server/core/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Character',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Game',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='character',
+            name='game',
+            field=models.ForeignKey(to='core.Game'),
+        ),
+        migrations.AddField(
+            model_name='character',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+    ]
--- gom_server/core/models.py
+++ gom_server/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+# Class for a Game
+class Game(models.Model):
+   name = models.CharField(max_length=50)
+
+# Class for a Character
+class Character(models.Model):
+   user = models.ForeignKey(User)
+   game = models.ForeignKey(Game)
+   name = models.CharField(max_length=200)
