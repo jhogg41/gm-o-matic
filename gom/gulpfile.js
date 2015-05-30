73c7e77edf3b246ada7241a0eb80caf10537c77b
Jonathan Hogg <jhogg41@gmail.com>
2015-05-30 17:22:22 +0100

    Add SASS support

--- gom/gulpfile.js
+++ gom/gulpfile.js
@@ -1,8 +1,37 @@
 // grab our gulp packages
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+var gulp       = require('gulp'),
+    jshint     = require('gulp-jshint'),
+    sass       = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
 
-// create a default task and just log a message
-gulp.task('default', function() {
-   return gutil.log('Gulp is running!')
+// define the default task and add watch to it
+gulp.task('default', ['watch']);
+
+// jshint lint
+gulp.task('jshint', function() {
+   return gulp.src([
+         'app/**/*.js',
+         '!app/bower_components/**'
+      ])
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// sass compilation
+gulp.task('build-css', function() {
+   return gulp.src('app/css/**/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('app/css'));
+});
+
+// watchers
+gulp.task('watch', function() {
+   gulp.watch([
+                 'app/**/*.js',
+                 '!app/bower_components/**'
+              ],
+              ['jshint']);
+   gulp.watch(['app/css/**/*.scss'], ['build-css']);
 });
