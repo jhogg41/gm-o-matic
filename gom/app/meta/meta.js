0d26bf147707bde6832af1adde4e61375b18e408
Jonathan Hogg <jhogg41@gmail.com>
2015-05-31 17:14:04 +0000

    Sort out game switching dialog

--- gom/app/meta/meta.js
+++ gom/app/meta/meta.js
@@ -3,20 +3,54 @@
 angular.module('gomApp.meta', [
 ])
 
-.controller('MetaCtrl', ['$mdUtil', '$mdSidenav',
-function($mdUtil, $mdSidenav) {
+.service('MetaService', [function() {
+   var ms = this;
+   this.game = {
+      'name': 'Crisis'
+   };
+   this.switchGame = function(newName) {
+      // Called to switch game
+      ms.game.name = newName;
+   };
    this.user = {
       'username': 'jhogg',
       'activeChar': 'Fred Bloggs'
    };
-   this.activeGame = "Crisis";
    this.user.is_gm = function(character) {
       // Return true is this user has GM permissions on the game of given
       // character.
       return true;
    };
+}])
+
+.controller('MetaCtrl', ['$mdDialog', '$mdSidenav', '$mdUtil', 'MetaService',
+function($mdDialog, $mdSidenav, $mdUtil, MetaService) {
+   this.user = MetaService.user;
+   this.game = MetaService.game;
 
    this.toggleMenu = $mdUtil.debounce(function(){
       $mdSidenav('menu').toggle();
    });
+   this.gameSwitcher = function(){
+      $mdDialog.show({
+         clickOutsideToClose: 'true',
+         controller: 'GameSwitchCtrl',
+         controllerAs: 'gsw',
+         templateUrl: 'meta/gameSwitch.html',
+         })
+      .then(function(game) {
+         MetaService.switchGame(game);
+      });
+   };
+}])
+
+.controller('GameSwitchCtrl', ['$mdDialog', 'MetaService',
+function($mdDialog, MetaService) {
+   this.game = MetaService.game.name;
+   this.cancel = function() {
+      $mdDialog.cancel();
+   };
+   this.switch = function() {
+      $mdDialog.hide(this.game);
+   };
 }]);
