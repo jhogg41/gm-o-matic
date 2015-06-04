b0c21a2023bf0e43ff5f424eaa5d38023135066a
Jonathan Hogg <jhogg41@gmail.com>
2015-06-04 20:43:10 +0000

    Ensure cat and terms are agreed

--- gom_server/core/forms.py
+++ gom_server/core/forms.py
@@ -6,3 +6,5 @@ import django.forms
 class SignupForm(allauth.account.forms.SignupForm):
    first_name = django.forms.CharField(max_length=30,required=True)
    last_name = django.forms.CharField(max_length=30,required=True)
+   terms = django.forms.BooleanField(required=True) # Not saved to model
+   cat = django.forms.BooleanField(required=True) # Not saved to model
