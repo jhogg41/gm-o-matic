56166df13e70d6a1175d033cacf50a6cd04e788b
Jonathan Hogg <jhogg41@gmail.com>
2015-06-05 12:34:05 +0000

    poke character setup urls and skeleton

--- gom/app/character/character.js
+++ gom/app/character/character.js
@@ -3,11 +3,23 @@
 angular.module('gomApp.character', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/:game/:char/profile', {
+  $routeProvider.when('/character', {
     templateUrl: 'character/character.html',
     controller: 'CharacterCtrl',
     controllerAs: 'char'
   });
+}])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/character/new', {
+    templateUrl: 'character/newchar.html',
+    controller: 'NewCharacterCtrl',
+    controllerAs: 'char'
+  });
+}])
+
+.controller('NewCharacterCtrl', [function() {
+
 }])
 
 .controller('CharacterCtrl', [function() {
